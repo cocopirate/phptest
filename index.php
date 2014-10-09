282a4687c47758bec530433a1e9b2b243fad915e
cocopirate <949570341@qq.com>
2014-10-09 20:56:30 +0800

    创建

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+<html lang="CN_zh">
+<head>
+	<title>php测试</title>
+</head>
+<body>
+<?php 
+$fp = fopen('text.txt', 'r');
+$order = fgetcsv($fp,100,'-');
+echo $order[1];
+?>
+</body>
+</html>
